c861fe2d793508330f9a2aabe9053907615aa859
Ayush suwalka <suwalkaayush1910@.com>
2026-04-09 18:37:48 +0530

    initial commit

---
+++ Conceptual_questions/Find_max_ele.py
@@ -0,0 +1,18 @@
+# First method
+arr = [10, 25, 5, 78, 30]
+
+max_element = arr[0]   # assume first element max hai
+
+for num in arr:
+    if num > max_element:
+        max_element = num
+
+print("Maximum element:", max_element)
+
+
+# Second method(using built-in function))
+arr = [10, 25, 5, 78, 30]
+
+max_element = max(arr)
+
+print("Maximum element:", max_element)
